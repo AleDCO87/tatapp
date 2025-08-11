b1f0d280b157de06afa15d276d5ff6b80f8462aa
Ale <aledco@ug.uchile.cl>
2025-08-10 20:00:54 -0400

    prueba home

--- app/src/main/java/com/example/tatapp/ui/screens/home/Home.kt
+++ app/src/main/java/com/example/tatapp/ui/screens/home/Home.kt
@@ -1,7 +1,24 @@
 package com.example.tatapp.ui.screens.home
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 
 @Composable
-fun Home( navController: NavHostController) {}
+fun Home( navController: NavHostController) {
+    Scaffold ( modifier=Modifier.fillMaxSize()) { innerPadding ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {item {Text(text="Hola")}
+
+        }
+
+    }
+}
