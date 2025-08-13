6d025c16795690dca4586ebbf40a17230ff531d5
Ale <aledco@ug.uchile.cl>
2025-08-13 18:32:35 -0400

    splashscreen completo

--- app/src/main/java/com/example/tatapp/MainActivity.kt
+++ app/src/main/java/com/example/tatapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -14,7 +15,10 @@ import com.example.tatapp.ui.theme.TatappTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        splashScreen.setKeepOnScreenCondition { false }
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContent {
             TatappTheme {
