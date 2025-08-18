97df46fba9bc1c3ce850e79a48abe3b5b1e84d4d
Ale <aledco@ug.uchile.cl>
2025-08-18 13:08:35 -0400

    splashscreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.compose.material)
     implementation(libs.material3)
     implementation(libs.androidx.foundation)
+    implementation(libs.androidx.splashscreen)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
